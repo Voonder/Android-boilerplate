f16b387ef17d04e0fb4a6735aa1dbc5d90e7af6b
Julien NORMAND <julien.normand.92@gmail.com>
2017-03-28 01:21:59 +0200

    Insert default greenDAO on App.java

--- app/src/main/java/com/voonapp/boilerplate/App.java
+++ app/src/main/java/com/voonapp/boilerplate/App.java
@@ -6,6 +6,10 @@ import com.squareup.leakcanary.LeakCanary;
 
 public class App extends Application {
 
+//	public static final boolean ENCRYPTED = true;
+//
+//	private DaoSession daoSession;
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -14,5 +18,13 @@ public class App extends Application {
 		}
 
 		LeakCanary.install(this);
+
+//		DevOpenHelper helper = new DevOpenHelper(this, ENCRYPTED ? "notes-db-encrypted" : "notes-db");
+//		Database db = ENCRYPTED ? helper.getEncryptedWritableDb("super-secret") : helper.getWritableDb();
+//		daoSession = new DaoMaster(db).newSession();
 	}
+
+//	public DaoSession getDaoSession() {
+//		return daoSession;
+//	}
 }
