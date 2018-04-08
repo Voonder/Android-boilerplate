cd7890396e0d8f38a4d39ed151cd5063f54d899e
Julien NORMAND <julien.normand.92@gmail.com>
2018-04-08 16:35:44 +0200

    Update IDEA configuration files from AS 3.1

---
+++ .idea/fileTemplates/includes/Javadoc Header.java
@@ -0,0 +1,8 @@
+#parse("_Variables.java")
+/**
+ * ${NAME} description
+ * 
+ * @author ${author} - ${company} <a href="mailto:${email}">${email}</a> 
+ * @version ${version}
+ * @since ${YEAR}-${MONTH}-${DAY}
+ */
---
+++ .idea/fileTemplates/includes/KDoc Header.kt
@@ -0,0 +1,8 @@
+#parse("_Variables.java")
+/**
+ * ${NAME} description
+ * 
+ * @author ${author} - ${company} [${email}](${email}) 
+ * @version ${version}
+ * @since ${YEAR}-${MONTH}-${DAY}
+ */
